94309a721f251f60de5732074f6fd255afb5047d
VictorNicolasLO <victornicolaslo@hotmail.com>
2019-09-24 15:43:00 -0600

    Done

---
+++ src/components/test-component/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '@bit/semantic-org.semantic-ui-react.button';
+function TestComponent() {
+  return (
+    <div>
+      <Button>Hola</Button>
+    </div>
+  );
+}
+
+export default TestComponent;
